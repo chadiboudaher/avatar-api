17276cb1a69d4fafe974cafbf8259c6e9efe132d
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-16 09:45:02 +0300

    feat: add Fake Database list

---
+++ fake_db.py
@@ -0,0 +1,65 @@
+fake_database = [
+    {
+        "id": 1,
+        "name": "Korra",
+        "bio": "Korra was the Water Tribe-born Avatar succeeding Avatar Aang and preceding Avatar Pavi.",
+        "nation": "water",
+        "is_bender": True,
+        "show": "Legend of Korra",
+        "first_appearance":"LOK: Episode 1"
+    },
+    {
+        "id": 2,
+        "name": "Aang",
+        "bio": "Aang was the Air Nomad Avatar succeeding Avatar Roku and preceding Avatar Korra.",
+        "nation": "air",
+        "is_bender": True,
+        "show": "Avatar The Last Airbender",
+        "first_appearance":"ATLA: Episode 1"
+    },
+    {
+        "id": 3,
+        "name": "Sokka",
+        "bio": "Sokka was a non-bending warrior and strategist from the Southern Water Tribe, and a founding member of Team Avatar.",
+        "nation": "water",
+        "is_bender": False,
+        "show": "Avatar The Last Airbender",
+        "first_appearance": "ATLA: Episode 1"
+    },
+    {
+        "id": 4,
+        "name": "Zuko",
+        "bio": "Zuko was the banished Fire Nation prince who became Fire Lord after helping defeat his father, Ozai.",
+        "nation": "fire",
+        "is_bender": True,
+        "show": "Avatar The Last Airbender",
+        "first_appearance": "ATLA: Episode 1"
+    },
+    {
+        "id": 5,
+        "name": "Toph",
+        "bio": "Toph was a blind earthbending prodigy who invented metalbending and later founded the Metalbending Academy.",
+        "nation": "earth",
+        "is_bender": True,
+        "show": "Avatar The Last Airbender",
+        "first_appearance": "ATLA: Episode 6"
+    },
+    {
+        "id": 6,
+        "name": "Bolin",
+        "bio": "Bolin was an earthbender from Republic City and a professional pro-bending athlete alongside his brother Mako.",
+        "nation": "earth",
+        "is_bender": True,
+        "show": "Legend of Korra",
+        "first_appearance": "LOK: Episode 1"
+    },
+    {
+        "id": 7,
+        "name": "Ty Lee",
+        "bio": "Ty Lee was a cheerful and energetic girl who hailed from the Fire Nation.",
+        "nation": "fire",
+        "is_bender": False,
+        "show": "Avatar The Last Airbender",
+        "first_appearance": "ATLA: Season 2, Episode 3"
+    }
+]
