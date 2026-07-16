0a4fba2102088ac7647f5e3c191651497e2413f7
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-16 09:13:37 +0300

    feat: add CharacterBase schema with Nation/Show enums

---
+++ models.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from typing import Optional, Annotated
+from enum import Enum
+
+class Nation(Enum):
+    EARTH = "earth"
+    WATER = "water"
+    FIRE = "fire"
+    AIR = "air"
+
+class Show(Enum):
+    ATLA = "Avatar The Last Airbender"
+    LOK = "Legend of Korra"
+
+class CharacterBase(BaseModel):
+    name: str
+    bio: Optional[str] = None
+    nation: Nation
+    is_bender: bool
+    show: Show
+    first_appearance: Optional[str] = None
