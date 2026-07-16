69d1cca94ccc96bc0a40fa5629971600bc1c321d
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-16 10:16:35 +0300

    feat: add GET /characters/{id} endpoint with 404 handling

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
 from models import CharacterOut, CharacterBase, CharacterCreate
 from fake_db import fake_database
 
@@ -11,3 +11,12 @@ async def root():
 @app.get("/characters", response_model=list[CharacterOut])
 async def get_characters():
     return fake_database
+
+@app.get("/characters/{character_id}", response_model=CharacterOut)
+async def get_character(character_id: int):
+    character = next((item for item in fake_database 
+                 if item["id"] == character_id), None)
+    if character is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Character Not Found")
+    return character
