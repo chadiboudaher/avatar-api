97c935397221bd5d811120fe85ef0abc4a4610a7
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-16 10:06:27 +0300

    feat: add GET all endpoints for users

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from models import CharacterOut, CharacterBase, CharacterCreate
+from fake_db import fake_database
 
-app = FastAPI()
+app = FastAPI(title="Avatar API")
 
 @app.get("/")
 async def root():
     return {"message": "Hello"}
+
+@app.get("/characters", response_model=list[CharacterOut])
+async def get_characters():
+    return fake_database
