2a01578093ec6a91775ee028c889ea546d35b6b6
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-17 12:19:56 +0300

    feat: add SessionLocal and explain code

--- database.py
+++ database.py
@@ -1,6 +1,11 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 engine = create_engine("sqlite:///./avatar.db", 
                        connect_args={
                            "check_same_thread": False
                        })
+
+SessionLocal = sessionmaker(autocommit=False,
+                            autoFlush=False,
+                            bind=engine)
