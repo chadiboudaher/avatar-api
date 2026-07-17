09b02fbeea5ba49c523fb63709ed17259adb98e2
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-17 12:31:53 +0300

    feat: add base class for central registry

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 engine = create_engine("sqlite:///./avatar.db", 
                        connect_args={
@@ -9,3 +9,20 @@ engine = create_engine("sqlite:///./avatar.db",
 SessionLocal = sessionmaker(autocommit=False,
                             autoFlush=False,
                             bind=engine)
+
+"""
+`Declarative` is thr foudantional base class used to create a central
+registry for all SQLalchemy ORM models. Every database table class
+inherit from it, acting as a "container" or "module" that groups
+models together.
+"""
+class Base(DeclarativeBase):
+    pass
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close
