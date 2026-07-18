633a9ff0ab2aa219247025a8afabddf7481461ed
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-18 12:13:45 +0300

    feat: add NationOut schema and connect to CharacterOut

--- schemas.py
+++ schemas.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, ConfigDict
 from typing import Optional
-from enums import Nation, Show
+from enums import Show
 
 class CharacterBase(BaseModel):
     # allows pydantic models to read data from objects attributes
@@ -8,13 +8,25 @@ class CharacterBase(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     name: str
     bio: Optional[str] = None
-    nation: Nation
+    nation_id: int
     is_bender: bool
     show: Show
     first_appearance: Optional[str] = None
 
+class NationOut(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
+    name: str
+
 class CharacterCreate(CharacterBase):
     pass
 
-class CharacterOut(CharacterBase):
+class CharacterOut(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
     id: int
+    name: str
+    bio: Optional[str] = None
+    nation: NationOut
+    is_bender: bool
+    show: Show
+    first_appearance: Optional[str] = None
