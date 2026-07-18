7a88c4801e37c7b513350ab1e7f95dcaa794f2cc
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-18 11:49:16 +0300

    feat: add seperate nations database & connect to characters

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from database import Base
-from enums import Nation, Show
-from sqlalchemy import Enum as SQLEnum
-from sqlalchemy.orm import Mapped, mapped_column
+from enums import Show
+from sqlalchemy import Enum as SQLEnum, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 class Character(Base):
     __tablename__ = "characters"
@@ -9,7 +9,16 @@ class Character(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(unique=True)
     bio: Mapped[str] = mapped_column(nullable=True)
-    nation: Mapped[Nation] = mapped_column(SQLEnum(Nation))
+    nation_id: Mapped[int] = mapped_column(ForeignKey("nations.id"))
+    nation: Mapped["Nation"] = relationship(back_populates="characters")
     is_bender: Mapped[bool]
     show: Mapped[Show] = mapped_column(SQLEnum(Show))
     first_appearance: Mapped[str] = mapped_column(nullable=True)
+
+
+class Nation(Base):
+    __tablename__ = "nations"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True)
+    characters: Mapped[list["Character"]] = relationship(back_populates="nation")
