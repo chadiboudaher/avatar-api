1619fd2b03a6ae89e5dd1d47bc39aa77ea46463e
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-18 12:00:10 +0300

    feat: POST /nations to insert nation options

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, status, HTTPException, Depends
 from sqlalchemy.orm import Session
 
 from database import engine, get_db, Base
-from models import Character
+from models import Character, Nation
 from schemas import CharacterOut, CharacterCreate
 
 Base.metadata.create_all(bind=engine)
@@ -13,12 +13,20 @@ app = FastAPI(title="Avatar API")
 async def root():
     return {"message": "Hello"}
 
-@app.get("/characters", response_model=list[CharacterOut])
+@app.post("/nations", status_code=status.HTTP_201_CREATED, tags=["Nations"])
+async def create_nations(db: Session = Depends(get_db)):
+    nation_names = ['water', "earth", "fire", "air"]
+    nations = [Nation(name=n) for n in nation_names]
+    db.add_all(nations)
+    db.commit()
+    return None
+
+@app.get("/characters", response_model=list[CharacterOut], tags=["Characters"])
 async def get_all(db: Session = Depends(get_db)):
     characters = db.query(Character).all()
     return characters
 
-@app.get("/characters/{character_id}", response_model=CharacterOut)
+@app.get("/characters/{character_id}", response_model=CharacterOut, tags=["Characters"])
 async def get_characters(character_id: int,
                          db: Session = Depends(get_db)):
     character = db.query(Character).filter(Character.id == character_id).first()
@@ -27,7 +35,7 @@ async def get_characters(character_id: int,
                             detail="Character Not Found")
     return character
 
-@app.post("/characters", response_model=CharacterOut)
+@app.post("/characters", response_model=CharacterOut, tags=["Characters"])
 async def create_character(character: CharacterCreate,
                            db: Session = Depends(get_db)):
     existing = db.query(Character).filter(Character.name == character.name).first()
@@ -40,7 +48,7 @@ async def create_character(character: CharacterCreate,
     db.refresh(new_character)
     return new_character
 
-@app.put("/characters/{character_id}", response_model=CharacterOut)
+@app.put("/characters/{character_id}", response_model=CharacterOut, tags=["Characters"])
 async def update_character(character_id: int,
                            character: CharacterCreate,
                            db: Session = Depends(get_db)):
@@ -54,7 +62,7 @@ async def update_character(character_id: int,
     db.refresh(existing)
     return existing
 
-@app.delete("/characters/{character_id}", status_code=status.HTTP_204_NO_CONTENT)
+@app.delete("/characters/{character_id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Characters"])
 async def delete_character(character_id: int,
                            db: Session = Depends(get_db)):
     existing = db.query(Character).filter(Character.id == character_id).first()
