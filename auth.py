c2755ae28550d10e42606b53e916032fe779d93c
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-19 14:24:40 +0300

    feat: add create_access_token function

--- auth.py
+++ auth.py
@@ -1,1 +1,14 @@
 import jwt
+from jwt import PyJWTError
+from datetime import datetime, timedelta, timezone
+
+SECRET_KEY = "your-secret-key-change-this"
+ALGORITHM = "HS256"
+ACESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict,
+                        expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=ACESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
